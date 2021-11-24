3abf0a028ef706ec2248f3e3e39c1739772ac8b9
vanchung0100 <85282793+vanchung0100@users.noreply.github.com>
2021-11-24 18:11:47 +0700

    Update game.py

--- game.py
+++ game.py
@@ -14,32 +14,32 @@ screen = pygame.display.set_mode((800,600))
 background = pygame.image.load('background.png')
 
 # you
-haiImg = []
-haiX = []
-haiY = []
-haiX_change = []
-haiY_change = []
-num_of_hai = 5
+ChungImg = []
+ChungX = []
+ChungY = []
+ChungX_change = []
+ChungY_change = []
+num_of_chung = 5
 
 for i in range(num_of_hai):
-    haiImg.append(pygame.image.load('hai.png'))
-    haiX.append(random.randint(0,735))
-    haiY.append(random.randint(20,150))
-    haiX_change.append(0.2)
-    haiY_change.append(20)
+    ChungImg.append(pygame.image.load('chung.png'))
+    ChungX.append(random.randint(0,735))
+    ChungY.append(random.randint(20,150))
+    ChungX_change.append(0.2)
+    ChungY_change.append(20)
 
-    def hai(x,y,i):
-        screen.blit(haiImg[i],(x,y))
+    def chung(x,y,i):
+        screen.blit(chungImg[i],(x,y))
 
 # your girl friend
-nganImg = pygame.image.load('ngan.png')
-nganX = 380
-nganY = 500
-nganX_change = 0
-nganY_change = 0
+TrangImg = pygame.image.load('trang.png')
+TrangX = 380
+TrangY = 500
+TrangX_change = 0
+TrangY_change = 0
 
-def ngan(x,y):
-    screen.blit(nganImg,(x,y))
+def trang(x,y):
+    screen.blit(trangImg,(x,y))
 
 # heart
 heartImg = pygame.image.load('heart.png')
@@ -61,8 +61,8 @@ icon = pygame.image.load('heart.png')
 pygame.display.set_icon(icon)
 
 # check colision
-def iscollision(haiX,haiY,heartX,heartY):
-    distance = math.sqrt(math.pow(haiX - heartX,2)+math.pow(haiY - heartY,2))
+def iscollision(chungX,chungY,heartX,heartY):
+    distance = math.sqrt(math.pow(chungX - heartX,2)+math.pow(chungY - heartY,2))
 
     if distance < 27:
         return True
@@ -100,69 +100,69 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                nganX_change = 1
+                TrangX_change = 1
             if event.key == pygame.K_LEFT:
-                nganX_change = -1
+                TrangX_change = -1
             if event.key == pygame.K_UP:
-                nganY_change = -1
+                TrangY_change = -1
             if event.key == pygame.K_DOWN:
-                nganY_change = 1
+                TrangY_change = 1
             if event.key == pygame.K_SPACE:
                 if heart_state == "ready":
-                    heartX = nganX
-                    heartY = nganY
+                    heartX = trangX
+                    heartY = trangY
                     heart(heartX,heartY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                nganX_change = 0
+                TrangX_change = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                nganY_change = 0
+                TrangY_change = 0
 
-    ngan(nganX,nganY)
-    nganX += nganX_change
-    nganY += nganY_change
+    Trang(trangX,trangY)
+    TrangX += trangX_change
+    TrangY += trangY_change
 
-    if nganX <=0:
-        nganX = 0 
-    elif nganX >=736:
-        nganX = 736
+    if trangX <=0:
+        TrangX = 0 
+    elif trangX >=736:
+        TrangX = 736
 
-    if nganY <= 400: 
-        nganY = 400
-    elif nganY >=530:
-        nganY = 529
+    if trangY <= 400: 
+        TrangY = 400
+    elif trangY >=530:
+        TrangY = 529
 
-    for i in range(num_of_hai):
+    for i in range(num_of_chung):
         # game over
-        if haiY[i] > 200:
-            for j in range(num_of_hai):
-                haiY[j] =2000
+        if chungY[i] > 200:
+            for j in range(num_of_chung):
+                ChungY[j] =2000
             game_over_text()
             break
 
-        hai(haiX[i],haiY[i],i)
+        Chung(chungX[i],chungY[i],i)
 
-        if haiX[i] <= 0:
-            haiX_change[i] = 0.2
-            haiY[i] += haiY_change[i]
-        if haiX[i] >= 736:
-            haiX_change[i] = -0.2
-            haiY[i] += haiY_change[i]
+        if chungX[i] <= 0:
+            ChungX_change[i] = 0.2
+            ChungY[i] += chungY_change[i]
+        if chungX[i] >= 736:
+            ChungX_change[i] = -0.2
+            ChungY[i] += chungY_change[i]
 
-        haiX[i] += haiX_change[i]
+        ChungX[i] += chungX_change[i]
            
-        collision = iscollision(haiX[i],haiY[i],heartX,heartY)
+        collision = iscollision(chungX[i],chungY[i],heartX,heartY)
         if collision:
             heart_state = "ready"
-            haiY[i] = random.randint(50,150)
-            haiX[i] = random.randint(0,735)
+            ChungY[i] = random.randint(50,150)
+            ChungX[i] = random.randint(0,735)
             score_value += 1
             explosion_sound = mixer.Sound('tick.wav')
             explosion_sound.play()
 
     if heartY <= 0:
-        heartY = nganY
+        heartY = trangY
         heart_state ="ready" 
     if heart_state == "fire":
         heartY -= heartY_change
